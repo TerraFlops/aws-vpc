8cd25058ff1e9b4f8eaf7d9c8a1b07955267eda0
Damian Sloane <damian@capacitorlabs.com.au>
2021-03-17 12:33:03 +1100

    Work in progress

--- modules/flow_logs/module.tf
+++ modules/flow_logs/module.tf
@@ -41,7 +41,7 @@ resource "aws_iam_role_policy_attachment" "flow_log_iam_role_policy_attachment" 
 
 resource "aws_cloudwatch_log_group" "flow_log_cloudwatch_log_group" {
   name = var.log_group_name
-  retention_in_days = 0
+  retention_in_days = 3653
   kms_key_id = var.flow_log_kms_key_id
 }
 
